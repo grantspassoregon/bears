8aa5f876d37f53dc9d5a2e27de2a62a5f479d920
Erik Rose <erik.w.rose@gmail.com>
2025-09-22 09:57:07 -0700

    feat: `FromStr` and `Display` impls added to `Footnotes` types. A `description` method added linking the variant to the BEA description.

--- bears_species/src/keys/footnote.rs
+++ bears_species/src/keys/footnote.rs
@@ -1,3 +1,8 @@
+use crate::{
+    BeaErr, DeriveFromStr, MneDoi, ParameterFields, ParameterName, ParameterValueTable,
+    ParameterValueTableVariant,
+};
+
 #[derive(
     Debug,
     Default,
@@ -9,6 +14,8 @@
     Hash,
     serde::Deserialize,
     serde::Serialize,
+    derive_more::FromStr,
+    derive_more::Display,
     strum::EnumIter,
 )]
 pub enum Footnotes {
@@ -18,12 +25,64 @@ pub enum Footnotes {
 }
 
 impl Footnotes {
+    #[tracing::instrument(skip_all)]
+    pub fn description(&self) -> &'static str {
+        match self {
+            Self::Yes => "footnotes are included",
+            Self::No => "footnotes are not included",
+        }
+    }
+
+    #[tracing::instrument(skip_all)]
+    pub fn key(&self) -> &'static str {
+        match self {
+            Self::Yes => "yes",
+            Self::No => "no",
+        }
+    }
+
+    #[tracing::instrument(skip_all)]
     pub fn params(&self) -> (String, String) {
-        let key = "GetFootnotes".to_string();
-        let value = match self {
-            Self::Yes => "yes".to_string(),
-            Self::No => "no".to_string(),
-        };
+        let key = ParameterName::GetFootnotes.to_string();
+        let value = self.key().to_owned();
         (key, value)
+    }
+}
+
+use std::str::FromStr;
+impl TryFrom<&ParameterFields> for Footnotes {
+    type Error = DeriveFromStr;
+
+    fn try_from(value: &ParameterFields) -> Result<Self, Self::Error> {
+        Self::from_str(value.key()).map_err(|e| {
+            DeriveFromStr::new(value.key().to_owned(), e, line!(), file!().to_string())
+        })
+    }
+}
+
+impl TryFrom<&MneDoi> for Footnotes {
+    type Error = DeriveFromStr;
+    fn try_from(value: &MneDoi) -> Result<Self, Self::Error> {
+        Self::from_str(value.key()).map_err(|e| {
+            DeriveFromStr::new(value.key().to_owned(), e, line!(), file!().to_string())
+        })
+    }
+}
+
+impl TryFrom<&ParameterValueTable> for Footnotes {
+    type Error = BeaErr;
+    fn try_from(value: &ParameterValueTable) -> Result<Self, Self::Error> {
+        match value {
+            ParameterValueTable::ParameterFields(pf) => Ok(Self::try_from(pf)?),
+            ParameterValueTable::MneDoi(tab) => Ok(Self::try_from(tab)?),
+            other => {
+                let error = ParameterValueTableVariant::new(
+                    format!("ParameterFields or MneDoi needed, found {other:#?}"),
+                    line!(),
+                    file!().to_string(),
+                );
+                Err(error.into())
+            }
+        }
     }
 }
